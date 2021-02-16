79fe8fdf1c7b550ba0bfb2e46609824607bacace
Nezio <nemanja1994@gmail.com>
2021-02-16 23:33:34 +0100

    Add general loss logic, content loss and config file

---
+++ config.py
@@ -0,0 +1,42 @@
+
+# content image path
+content_image_path = "images/wave.png"
+
+# style image path
+style_image_path = "images/wave.png"
+
+# generated image path
+generated_image_path = content_image_path
+
+# width of the generated (output) image; the height is calculated automatically
+image_width = 512
+
+
+# learning rate
+learning_rate = 10
+
+
+# number of iterations to run
+iterations = 1000
+
+# name of a layer from pre-trained CNN to extract content activations from
+content_layer_name = "block5_conv2"
+
+# names of layers from pre-trained CNN to extract style activations from
+style_layer_names = [
+    "block1_conv1",
+    "block2_conv1",
+    "block3_conv1",
+    "block4_conv1",
+    "block5_conv1",
+]
+
+
+# content weight (alpha)
+content_weight = 0.0000001
+
+# style weight (beta)
+style_weight = 0.0000001
+
+# total variation weight
+total_variation_weight = 0.0000001
--- main.py
+++ main.py
@@ -5,17 +5,9 @@ from tensorflow import keras
 from tensorflow.keras.applications import vgg19
 import numpy as np
 
+import config
+
 def main():
-    # config
-    content_image_path = keras.utils.get_file("paris.jpg", "https://i.imgur.com/F28w3Ac.jpg")
-    style_image_path = "images/wave.png"
-    generated_image_path = content_image_path
-
-    # generated image size
-    width_content, height_content = keras.preprocessing.image.load_img(content_image_path).size
-    image_width = 512
-    image_height = int(image_width / width_content * height_content)
-
     # Tensorflow fix for the GPU memory allcation issue
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
@@ -41,37 +33,94 @@ def main():
     model = keras.Model(inputs=external_model.inputs, outputs=external_model_layers)
 
     # set optimizer
-    optimizer = keras.optimizers.Adam(learning_rate=10)
+    optimizer = keras.optimizers.Adam(learning_rate=config.learning_rate)
     
-    # load images
-    content_image = preprocess_image(content_image_path, image_width, image_height)
-    style_image = preprocess_image(style_image_path, image_width, image_height)
-    generated_image = tf.Variable(preprocess_image(generated_image_path, image_width, image_height))
+    # set generated image size
+    content_image_width, content_image_height = keras.preprocessing.image.load_img(config.content_image_path).size
+    image_width = config.image_width
+    image_height = int(image_width / content_image_width * content_image_height)
+
+    # load and preprocess images
+    content_image = load_image(config.content_image_path, image_width, image_height)
+    style_image = load_image(config.style_image_path, image_width, image_height)
+    generated_image = tf.Variable(load_image(config.generated_image_path, image_width, image_height))
 
     # TODO: try getting activations for content and style image before the loop
 
     # training loop
-        # loss
-        # gradients
+    for i in range(1, config.iterations + 1):
+        # get loss and gradients
+        loss, gradients = get_loss_and_gradients(content_image, style_image, generated_image, model)
+
         # optimize
         # log and output
 
     # final results
     x = 42
+
+##############################################################################################################################################
+##############################################################################################################################################
         
 def print_log(text):
-    print("[" + datetime.datetime.now().strftime("%H:%M:%S") + "]", end="")
+    print("[" + datetime.datetime.now().strftime("%Y.%m.%d. %H:%M:%S") + "]", end="")
     print(" " + text)
 
-def preprocess_image(image_path, image_width, image_height):
+def load_image(image_path, image_width, image_height):
     image = keras.preprocessing.image.load_img(image_path, target_size=(image_width, image_height))
     image = keras.preprocessing.image.img_to_array(image)
     image = np.expand_dims(image, axis=0)
     image = vgg19.preprocess_input(image)
     image = tf.convert_to_tensor(image)
+
     return image
 
+#@tf.function # Compiles a function into a callable TensorFlow graph.
+def get_loss_and_gradients(content_image, style_image, generated_image, model):
+    with tf.GradientTape() as gt:
+        loss = get_loss(content_image, style_image, generated_image, model)
 
+    gradients = gt.gradient(loss, generated_image)
+
+    return loss, gradients
+
+def get_loss(content_image, style_image, generated_image, model):
+    
+    # get all activations
+    input_tensor = tf.concat([content_image, style_image, generated_image], axis=0)
+    all_activations = model(input_tensor)
+
+    # calculate content loss
+    content_layer_activations = all_activations[config.content_layer_name]
+    content_image_activations = content_layer_activations[0,:,:,:]
+    generated_image_activations = all_activations[2,:,:,:]
+    content_loss = config.content_weight * get_content_loss(content_image_activations, generated_image_activations)
+
+    # calculate style loss
+    style_loss = tf.zeros(shape=())
+    style_weight = config.style_weight / len(config.style_layer_names)
+    image_width = content_image.shape[1]
+    image_height = content_image.shape[2]
+    for style_layer_name in config.style_layer_names:
+        style_layer_activations = all_activations[style_layer_name]
+        style_image_activations = style_layer_activations[1,:,:,:]
+        generated_image_activations = all_activations[2,:,:,:]
+        style_loss += style_weight * get_style_loss(content_image_activations, generated_image_activations, image_width, image_height)
+
+    # calculate total variation loss
+
+    # calculate final loss
+
+    return 12
+
+def get_content_loss(content_image_activations, generated_image_activations):
+    # TODO: added *0.5; check if it produces worse output image
+    loss = 0.5 * tf.reduce_sum(tf.square(generated_image_activations - content_image_activations))
+
+    return loss
+
+def get_style_loss(content_image_activations, generated_image_activations, image_width, image_height):
+    
+    x = 12
 
 
 if __name__ == "__main__":
